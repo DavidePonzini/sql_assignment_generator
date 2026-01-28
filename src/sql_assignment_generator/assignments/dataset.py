b0c4bcb9fe1009b024e5d0d86ff3ca331d571438
Davide Ponzini <davide.ponzini95@gmail.com>
2026-01-28 14:15:34 +0100

    refactored dataset to use catalog and new constraints

--- src/sql_assignment_generator/assignments/dataset.py
+++ src/sql_assignment_generator/assignments/dataset.py
@@ -1,13 +1,35 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
 import dav_tools
+
+from ..constraints.schema import SchemaConstraint
 from .. import llm
 import sqlglot
 from sql_error_taxonomy import SqlErrors
 from sqlscope import Catalog, build_catalog_from_sql
 from ..sql_errors_details import ERROR_DETAILS_MAP
 from ..difficulty_level import DifficultyLevel
-from ..constraints.schema import TableAmountConstraint, ColumnAmountConstraint, InsertAmountConstraint, HasCheckConstraint, HasSamePrimaryKeyConstraint
+from ..constraints import SchemaConstraint, schema as schema_constraints
+from ..exceptions import SQLParsingError
 
+
+default_constraints: dict[DifficultyLevel, Sequence[SchemaConstraint]] = {
+    DifficultyLevel.EASY: [
+        schema_constraints.tables.MinTables(2),
+        schema_constraints.tables.MinColumns(2, tables=1),
+        schema_constraints.values.MinRows(3)
+    ],
+    DifficultyLevel.MEDIUM: [
+        schema_constraints.tables.MinTables(4),
+        schema_constraints.tables.MinColumns(4, tables=2),
+        schema_constraints.values.MinRows(4)
+    ],
+    DifficultyLevel.HARD: [
+        schema_constraints.tables.MinTables(6),
+        schema_constraints.tables.MinColumns(5, tables=3),
+        schema_constraints.values.MinRows(5)
+    ]
+}
 
 @dataclass
 class Dataset:
@@ -64,158 +86,112 @@ SET search_path TO {schema};
 COMMIT;'''
     
     @staticmethod
-    def generate(domain: str, errors: list[tuple[SqlErrors, DifficultyLevel]]) -> 'Dataset':
+    def generate(domain: str,
+                 constraints: Sequence[SchemaConstraint],
+                 *,
+                 extra_details: list[str] = [],
+                 max_attempts: int = 3
+        ) -> 'Dataset':
         '''Generate a SQL dataset based on the specified parameters.'''
-        unique_schema_constraints_map = {}
-        constraint_ranks = {}
-        dataset_characteristics = []
-        
-        difficulty_order = {
-            DifficultyLevel.EASY: 1,
-            DifficultyLevel.MEDIUM: 2,
-            DifficultyLevel.HARD: 3
-        }
 
-        # control the maximum difficulty of errors
-        max_difficulty = max((difficulty for _, difficulty in errors), key=lambda d: difficulty_order[d])
-        max_rank = difficulty_order[max_difficulty]
+        # TODO: add default constraints based on difficulty level of errors
+        # they need to be handled in the calling function
 
-        if max_difficulty == DifficultyLevel.EASY:
-            minNumberOfTables = 2
-            minNumberOfColumnsXtables = 1
-            minNumberOfColumns = 2
-            minNumberOfInserts = 3
-        elif max_difficulty == DifficultyLevel.MEDIUM:
-            minNumberOfTables = 4
-            minNumberOfColumnsXtables = 2
-            minNumberOfColumns = 4
-            minNumberOfInserts = 4
-        else:  # HARD
-            minNumberOfTables = 6
-            minNumberOfColumnsXtables = 3
-            minNumberOfColumns = 5
-            minNumberOfInserts = 5
+        # merge similar constraints
+        constraints = schema_constraints.merge_constraints(constraints)
 
-        # mandatory constraints
-        base_constraints = [
-            TableAmountConstraint(minNumberOfTables),
-            ColumnAmountConstraint(minNumberOfColumnsXtables, minNumberOfColumns),
-            InsertAmountConstraint(minNumberOfInserts)
-        ]
-        
-        for c in base_constraints:
-            c_type = c.__class__.__name__
-            unique_schema_constraints_map[c_type] = c
-            constraint_ranks[c_type] = max_rank
-
-        # take other schema constraints inside error
-        for error, difficulty in errors:
-            error_details = ERROR_DETAILS_MAP[error]
-            all_constraints = error_details.constraints.get(difficulty, [])
-            current_rank = difficulty_order[difficulty]
-            
-            # check if constraints are in schema module
-            for constraint in all_constraints: 
-                if 'schema' in constraint.__class__.__module__:
-                    c_type = constraint.__class__.__name__
-                    
-                    # add or update constraint based on rank
-                    if c_type not in constraint_ranks or current_rank > constraint_ranks[c_type]:
-                        unique_schema_constraints_map[c_type] = constraint
-                        constraint_ranks[c_type] = current_rank
-
-            # check characteristics for dataset
-            if error_details.dataset_characteristics.strip():
-                dataset_characteristics.append(error_details.dataset_characteristics.strip())
-        
-        # we have all constraints
-        active_constraints = list(unique_schema_constraints_map.values())
-        formatted_constraints = '\n'.join(f'- {c.description}' for c in active_constraints)
+        formatted_constraints = '\n'.join(f'- {c.description}' for c in constraints)
         
         # dataset characteristics str
-        if len(dataset_characteristics) > 0:
-            dataset_characteristics_str = "The dataset must have the following characteristics:\n"
-            for characteristic in dataset_characteristics:
-                dataset_characteristics_str += f"- {characteristic}\n"
+        if len(extra_details) > 0:
+            extra_details_str = "The dataset must have the following characteristics:\n"
+            for detail in extra_details:
+                extra_details_str += f"- {detail}\n"
         else:
-            dataset_characteristics_str = ''
+            extra_details_str = ''
         
         prompt_text = f'''
-        Generate a SQL dataset about the following domain: "{domain}".
-        {dataset_characteristics_str}
+Generate a SQL dataset about the following domain: "{domain}".
+{extra_details_str}
 
-        MANDATORY CONSTRAINTS:
-        -FOREIGN KEY attributes should have the REFERENCES keyword inline (e.g. Col TYPE REFERENCES table_name(column_name)).
-        {formatted_constraints}
-        
-        MANDATORY OUTPUT (JSON):
-        {{
-            "schema_tables": ["CREATE TABLE t1...", "CREATE TABLE t2..."],
-            "insert_commands": ["INSERT INTO t1...", "INSERT INTO t2..."]
-        }}
+MANDATORY CONSTRAINTS:
+- FOREIGN KEY attributes should have the REFERENCES keyword inline (e.g. "col TYPE REFERENCES table_name(column_name)").
+{formatted_constraints}
 
-        MORE IMPORTANT the INSERT INTO must have following format (Multi-row insert): 
-        INSERT INTO tableName VALUES 
-            (val1, val2, ...),
-            (val3, val4, ...);
-        '''
-        
+MANDATORY OUTPUT (JSON):
+{{
+    "schema_tables": ["CREATE TABLE t1...", "CREATE TABLE t2..."],
+    "insert_commands": ["INSERT INTO t1...", "INSERT INTO t2..."]
+}}
+
+INSERT INTO statements must have following format (Multi-row insert): 
+INSERT INTO tableName VALUES 
+    (val_1, val_2, ...),
+    (val_n, val_n+1, ...);
+'''
+        # query LLM to generate dataset
         messages = llm.Message()
         messages.add_message_user(prompt_text)
         
-        max_attempts = 3
         for attempt in range(max_attempts):
             try:
-                json_risposta = llm.generate_answer(messages, json_format=llm.models.Schema) 
-                assert isinstance(json_risposta, llm.models.Schema), "The response is not in the expected JSON format."
+                answer = llm.generate_answer(messages, json_format=llm.models.Schema) 
+                assert isinstance(answer, llm.models.Schema), "The response is not in the expected JSON format."
 
-                #parsing CREATE TABLE
+                # parse CREATE TABLEs
                 parsed_tables = []
-                try:
-                    for cmd in json_risposta.schema_tables:
-                        parsed_tables.append(sqlglot.parse_one(cmd, read="postgres")) #parse convert string into AST expression list
-                except Exception as e:
-                    raise ValueError(f"Syntax error in CREATE TABLE generated: {e}")
+                for insert_into in answer.schema_tables:
+                    try:
+                        parsed = sqlglot.parse_one(insert_into, read="postgres")
+                        parsed_tables.append(parsed)
+                    except Exception as e:
+                        raise SQLParsingError(f"Syntax error in CREATE TABLE generated: {e}", insert_into)
+                create_commands = [f'{cmd.sql(pretty=True, dialect="postgres")};' for cmd in parsed_tables]
 
-                #parsing INSERT COMMANDS
+                # parse INSERT INTOs
                 parsed_inserts = []
-                try:
-                    for cmd in json_risposta.insert_commands:
-                        parsed_inserts.append(sqlglot.parse_one(cmd, read="postgres")) #parse_one convert string into AST (single object)
-                except Exception as e:
-                    raise ValueError(f"Syntax error in INSERT COMMANDS generated: {e}")
+                for insert_into in answer.insert_commands:
+                    try:
+                        parsed = sqlglot.parse_one(insert_into, read="postgres")
+                        parsed_inserts.append(parsed)
+                    except Exception as e:
+                        raise SQLParsingError(f"Syntax error in INSERT COMMANDS generated: {e}", insert_into)
+                insert_commands = [f'{cmd.sql(pretty=True, dialect="postgres")};' for cmd in parsed_inserts]
 
-                #constraint validation
-                missing_requirements = []
-                
-                for constraint in active_constraints:
-                    is_satisfied = False
+                catalog = build_catalog_from_sql('\n'.join(cmd.sql() for cmd in parsed_tables))
 
-                    if isinstance(constraint, InsertAmountConstraint):
-                        is_satisfied = constraint.validate(parsed_inserts, [])
-                    elif isinstance(constraint, (TableAmountConstraint, ColumnAmountConstraint, HasCheckConstraint, HasSamePrimaryKeyConstraint)):
-                         is_satisfied = constraint.validate(None, parsed_tables)
-                    else: is_satisfied = constraint.validate(parsed_inserts, parsed_tables)
-                    if not is_satisfied: missing_requirements.append(constraint.description)
+                # check if constraints are satisfied
+                errors = []
+                for constraint in constraints:
+                    if not constraint.validate(catalog, parsed_tables, parsed_inserts):
+                        errors.append(constraint.description)
 
-                #if all ok the dataset was created
-                if not missing_requirements:
+                # no errors, return dataset
+                if not errors:
                     dav_tools.messages.success(f"Dataset generated and validated successfully at attempt {attempt + 1}.")
-                    return Dataset(
-                        create_commands=[f'{cmd.sql(pretty=True, dialect="postgres")};' for cmd in parsed_tables],
-                        insert_commands=[f'{cmd.sql(pretty=True, dialect="postgres")};' for cmd in parsed_inserts],
+                    
+                    result = Dataset(
+                        create_commands=create_commands,
+                        insert_commands=insert_commands,
                         domain=domain
                     )
+                    # fill cache, since we already have the catalog
+                    result._catalog_cache = catalog
+                    result._catalog_cache_commands_hash = hash(tuple(create_commands))
+                    
+                    return result
                 
-                dav_tools.messages.error(f'Validation failed for attempt {attempt + 1}. Missing requirements: {", ".join(missing_requirements)}')
+                dav_tools.messages.error(f'Validation failed for attempt {attempt + 1}. Missing requirements: {", ".join(errors)}')
+                
+                # TODO: use constraint validation errors to give better feedback
                 feedback = (
-                    f"The previous JSON output was REJECTED because the SQL violated these constraints: {', '.join(missing_requirements)}. "
+                    f"The previous JSON output was REJECTED because the SQL violated these constraints: {', '.join(errors)}. "
                     "Regenerate the JSON correcting the SQL to satisfy ALL mandatory constraints."
                 )
                 messages.add_message_user(feedback)
 
-            except Exception as e:
+            except SQLParsingError as e:
                 dav_tools.messages.error(f"Error during generation (Attempt {attempt + 1}): {e}")
-                messages.add_message_user(f"An error occurred: {str(e)}. Please regenerate valid SQL.")
+                messages.add_message_user(f"SQL code is not syntactically valid: {str(e)}. Please regenerate valid SQL.")
         
         raise Exception(f'Failed to generate a valid dataset after {max_attempts} attempts.')
