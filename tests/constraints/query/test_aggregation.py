980f86aab4da56425ba30e7513394b2d1f679110
Davide Ponzini <davide.ponzini95@gmail.com>
2026-01-30 11:35:11 +0100

    parameterized test for constraint param

--- tests/constraints/query/test_aggregation.py
+++ tests/constraints/query/test_aggregation.py
@@ -22,20 +22,32 @@ def test_no_aggregation_pass():
     # Should not raise any exception
     constraint.validate(query)
 
-def test_aggregation_fail():
-    sql = "SELECT name, age FROM employees WHERE age > 30;"
+@pytest.mark.parametrize("sql,min_,max_,allowed_functions", [
+    ("SELECT name, age FROM employees WHERE age > 30;", 1, None, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, SUM(salary) FROM employees GROUP BY department;", 2, None, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, SUM(salary), COUNT(*) FROM employees GROUP BY department;", 1, 1, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, AVG(salary), MAX(age) FROM employees GROUP BY department;", 2, None, ['AVG', 'SUM']),
+    ("SELECT department, AVG(salary), MAX(age), COUNT(*) FROM employees GROUP BY department;", 1, 2, ['AVG', 'MAX', 'COUNT']),
+])
+def test_aggregation_fail(sql, min_, max_, allowed_functions):
     query = Query(sql)
 
-    constraint = Aggregation()
+    constraint = Aggregation(min_=min_, max_=max_, allowed_functions=allowed_functions)
 
     with pytest.raises(ConstraintValidationError) as exc_info:
         constraint.validate(query)
 
-def test_aggregation_pass():
-    sql = "SELECT department, SUM(salary) FROM employees GROUP BY department;"
+@pytest.mark.parametrize("sql,min_,max_,allowed_functions", [
+    ("SELECT department, SUM(salary) FROM employees GROUP BY department;", 1, None, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, AVG(salary), MAX(age) FROM employees GROUP BY department;", 2, None, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, SUM(salary), COUNT(*) FROM employees GROUP BY department;", 1, 2, ['AVG', 'SUM', 'COUNT', 'MIN', 'MAX']),
+    ("SELECT department, AVG(salary), MAX(age), COUNT(*) FROM employees GROUP BY department;", 3, 3, ['AVG', 'MAX', 'COUNT']),
+    ("SELECT department, AVG(salary), MAX(age), COUNT(*) FROM employees GROUP BY department;", 2, 2, ['AVG', 'MAX']),
+])
+def test_aggregation_pass(sql, min_, max_, allowed_functions):
     query = Query(sql)
 
-    constraint = Aggregation()
-
+    constraint = Aggregation(min_=min_, max_=max_, allowed_functions=allowed_functions)
+    
     # Should not raise any exception
     constraint.validate(query)
